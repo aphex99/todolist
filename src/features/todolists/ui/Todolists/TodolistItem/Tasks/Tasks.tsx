d48db2230dffdc462d2818e883cb866355ca17fc
belovroman90 <belov.roman.90@mail.ru>
2025-07-06 21:10:01 +0700

    fix TS in Tasks component. Add ? for data

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -1,53 +1,55 @@
-import {COUNT_PER_PAGE} from "@/common/constants";
-import {TaskStatus} from "@/common/enums";
-import {useGetTasksQuery} from "@/features/todolists/api/tasksApi";
-import type {DomainTodolist} from "@/features/todolists/lib/types";
+import { COUNT_PER_PAGE } from "@/common/constants"
+import { TaskStatus } from "@/common/enums"
+import { useGetTasksQuery } from "@/features/todolists/api/tasksApi"
+import type { DomainTodolist } from "@/features/todolists/lib/types"
 import {
-    TasksPagination
-} from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx";
-import List from "@mui/material/List";
-import {useState} from "react";
-import {TaskItem} from "./TaskItem/TaskItem";
-import {TasksSkeleton} from "./TasksSkeleton/TasksSkeleton";
+  TasksPagination
+} from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx"
+import List from "@mui/material/List"
+import { useState } from "react"
+import { TaskItem } from "./TaskItem/TaskItem"
+import { TasksSkeleton } from "./TasksSkeleton/TasksSkeleton"
 
 type Props = {
-    todolist: DomainTodolist
+  todolist: DomainTodolist
 }
 
-export const Tasks = ({todolist}: Props) => {
-    const {id, filter} = todolist;
+export const Tasks = ({ todolist }: Props) => {
+  const { id, filter } = todolist
 
-    const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1)
 
-    const {data, currentData, isLoading, isFetching} = useGetTasksQuery({todolistId: id, params: {page}});
+  const { data, isLoading } = useGetTasksQuery({ todolistId: id, params: { page } })
 
-    let filteredTasks = data?.items;
-    if (filter === "active") {
-        filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.New);
-    }
-    if (filter === "completed") {
-        filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.Completed);
-    }
+  let filteredTasks = data?.items
+  if (filter === "active") {
+    filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.New)
+  }
+  if (filter === "completed") {
+    filteredTasks = filteredTasks?.filter((task) => task.status === TaskStatus.Completed)
+  }
 
-    if (isLoading) {
-        return <TasksSkeleton/>;
-    }
+  if (isLoading) {
+    return <TasksSkeleton />
+  }
 
 
-    return (
+  return (
+    <>
+      {filteredTasks?.length === 0 ? (
+        <p>Тасок нет</p>
+      ) : (
         <>
-            {filteredTasks?.length === 0 ? (
-                <p>Тасок нет</p>
-            ) : (
-                <>
-                    <List>
-                        {filteredTasks?.map((task) => <TaskItem key={task.id} task={task} todolist={todolist}/>)}
-                    </List>
+          <List>
+            {filteredTasks?.map((task) => <TaskItem
+              key={task.id}
+              task={task} todolist={todolist} />)}
+          </List>
 
-                    {data.totalCount > COUNT_PER_PAGE &&
-                        <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage}/>}
-                </>
-            )}
+          {data?.totalCount > COUNT_PER_PAGE &&
+            <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage} />}
         </>
-    );
-};
+      )}
+    </>
+  )
+}
