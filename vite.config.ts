4865c07cb021ca633dcfeb410aefe88faf8d55f5
belovroman90 <belov.roman.90@mail.ru>
2025-07-07 12:49:11 +0700

    Change vite.config.ts base

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import react from "@vitejs/plugin-react-swc"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
-      "@/": `${path.resolve(__dirname, "src")}/`,
-    },
-  },
+      "@/": `${path.resolve(__dirname, "src")}/`
+    }
+  }
 })
